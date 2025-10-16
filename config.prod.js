12212b474feec44240471225761b22f8cb802ab5
Kris Brown <kristophert@pm.me>
2025-10-16 12:59:24 -0400

    Updating with my actual URLS

--- config.prod.js
+++ config.prod.js
@@ -1,10 +1,10 @@
 // Slack and Google Sheets Configuration for GitHub Pages
 window.SLACK_CONFIG = {
-    webhookUrl: 'YOUR_SLACK_WEBHOOK_URL_HERE',
-    googleSheetsUrl: 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE'
+    webhookUrl: 'https://hooks.slack.com/services/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',
+    googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbzWvnAUajYLi9Om0TkUuMlNJmOVbFoBlI_WC15ndPLv5iHfV3p9imeVN8QhlCg-osepUA/exec'
 };
 
 console.log('Config.prod.js loaded with URLs:', {
-    slack: window.SLACK_CONFIG.webhookUrl !== 'YOUR_SLACK_WEBHOOK_URL_HERE',
-    sheets: window.SLACK_CONFIG.googleSheetsUrl !== 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE'
+    slack: window.SLACK_CONFIG.webhookUrl !== 'https://hooks.slack.com/services/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',
+    sheets: window.SLACK_CONFIG.googleSheetsUrl !== 'https://script.google.com/macros/s/AKfycbzWvnAUajYLi9Om0TkUuMlNJmOVbFoBlI_WC15ndPLv5iHfV3p9imeVN8QhlCg-osepUA/exec'
 });
