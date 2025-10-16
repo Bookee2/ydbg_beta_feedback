c6db2da60aec5176b23c5bec3211d92c1ead308a
Kris Brown <kristophert@pm.me>
2025-10-16 13:23:13 -0400

    Fix Google Apps Script URL in config.prod.js

--- config.prod.js
+++ config.prod.js
@@ -1,7 +1,7 @@
 // Slack and Google Sheets Configuration for GitHub Pages
 window.SLACK_CONFIG = {
     webhookUrl: 'https://hooks.slack.com/services/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',
-    googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbzWvnAUajYLi9Om0TkUuMlNJmOVbFoBlI_WC15ndPLv5iHfV3p9imeVN8QhlCg-osepUA/exec'
+    googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbx086lKDETdRyuSSOzBRmmnR0idiEA1GGVjq-lHC9JRdvYwjbA9hA3hFaVfkaRpFl42Vw/exec'
 };
 
 console.log('Config.prod.js loaded with URLs:', {
