337c003cd87b1ac05af4e409c8e7fc5e98d36707
Kris Brown <KB@Kriss-MacBook-Pro.local>
2025-12-17 11:25:57 -0500

    Update Apps Script URL to new deployment

--- level2-5/config.prod.js
+++ level2-5/config.prod.js
@@ -1,6 +1,6 @@
 // Slack and Google Sheets Configuration for Levels 2-5
 window.SLACK_CONFIG = {
     webhookUrl: 'https://script.google.com/macros/s/AKfycbyMaWd6jPtC4wnIOYMD0Gywei-kH5qBm1JDfpe_NLCEst-LAwMAS1IoZekJqHgkvsNXmw/exec',
-    googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbxJQYqvVrYks3XwwB5F46pMWwOEo-pZjfvVVVAV8kZUElr9F0NSuv8L7YB2ka3dEff2AQ/exec'
+    googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbz41c9xbB8CePD8cYen-0qqF2qhAEW7MS6e6dtsVGQ1jNuiDP5oW_JPE3nWzjrkOPxVvw/exec'
 };
 
