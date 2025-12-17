28b081750ed0d7de6bd6d6f135ed5dc09890c3a3
Kris Brown <KB@Kriss-MacBook-Pro.local>
2025-12-17 11:30:31 -0500

    Update Apps Script URL to latest deployment with enhanced error handling

--- level2-5/config.prod.js
+++ level2-5/config.prod.js
@@ -1,6 +1,6 @@
 // Slack and Google Sheets Configuration for Levels 2-5
 window.SLACK_CONFIG = {
     webhookUrl: 'https://script.google.com/macros/s/AKfycbyMaWd6jPtC4wnIOYMD0Gywei-kH5qBm1JDfpe_NLCEst-LAwMAS1IoZekJqHgkvsNXmw/exec',
-    googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbz41c9xbB8CePD8cYen-0qqF2qhAEW7MS6e6dtsVGQ1jNuiDP5oW_JPE3nWzjrkOPxVvw/exec'
+    googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbwVwhL8jj9vGSD_9-9bSD41q-dtUJ1PM8yXfMTlSzxXdpArG7BXpetKv5SVKtly2F2tDA/exec'
 };
 
